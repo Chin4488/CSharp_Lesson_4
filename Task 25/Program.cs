f3edd00aa98552b085ffcb1eaee719c64b2fb791
«Igor <talovez@yandex.ru>
2022-03-29 23:02:01 +0300

    2 вариант, более верный

--- Task 25/Program.cs
+++ Task 25/Program.cs
@@ -5,10 +5,7 @@ Console.WriteLine("Введите число А");
 int NumberA = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число B");
 int NumberB = Convert.ToInt32(Console.ReadLine());
-int NumberC = 0;
-if (NumberB == 1) Console.WriteLine($"А в степени В = {NumberA}"); else
-{for (int i=1; i<NumberB; i++)
-    {NumberC = NumberC + NumberA * NumberA;
-}
+int NumberC = 1;
+for (int i=1; i<=NumberB; i++)
+NumberC = NumberC * NumberA;
 Console.WriteLine($"А в степени В = {NumberC}");
-}
